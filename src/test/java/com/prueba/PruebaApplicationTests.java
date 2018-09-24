610de9221c1bf92c387abf81e12a6c922f68d8a1
Juan Ramon Espinosa Lopez <espinosalopezjuanramon@gmail.com>
2018-09-23 20:57:23 -0500

    Se agregó i18n al proyecto

--- src/test/java/com/prueba/PruebaApplicationTests.java
+++ src/test/java/com/prueba/PruebaApplicationTests.java
@@ -1,16 +1,26 @@
 package com.prueba;
 
+import com.prueba.web.i18n.I18NService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PruebaApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private I18NService i18NService;
 
+	@Test
+	public void testMessageByLocaleService() throws Exception
+	{
+		String expecterResult="Bootstrap starter template";
+		String messageId="index.main.callout";
+		String actual=i18NService.getMessage(messageId);
+		org.junit.Assert.assertEquals("The actual and expected Strings don´t match", expecterResult, actual);
+	}
 }
